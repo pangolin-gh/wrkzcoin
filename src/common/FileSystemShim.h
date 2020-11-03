e0fd749fb54eb85db3957a77ab0e809b68f7e309
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-11-03 15:38:07 +0700

    Initial test for android

--- src/common/FileSystemShim.h
+++ src/common/FileSystemShim.h
@@ -5,14 +5,15 @@
 #pragma once
 
 /* Check if we have the <filesystem> header, or just <experimental/filesystem> */
+#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include) && !defined __ANDROID__
 #if __has_include(<filesystem>)
-
+#define GHC_USE_STD_FS
 #include <filesystem>
-
 namespace fs = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else
-#error Your compiler does not support either <filesystem> or <experimental/filesystem>. Please upgrade to one that does.
 #endif
+#endif
+#if defined(__ANDROID__)
+#include <ghc-filesystem/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
+
